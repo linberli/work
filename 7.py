c38ed9ae11518afe44c1bcc3a267d7954950e0a3
linberli <55867860+linberli@users.noreply.github.com>
2019-11-06 22:59:55 +0500

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,12 +1,8 @@
 import random
 >>> A= random.randint (1, 100)
 >>> print("Number:" + str(A))
-Number:42
 >>> if A % 2 == 0:
 	print ("The number is even")
-
-	
-The number is even
 >>> if A % 4 == 0:
 	print ("The number is a multiple of 4")
 
