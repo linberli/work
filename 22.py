bb6d43a5af6abcff9530096af762cd4604c28851
linberli <55867860+linberli@users.noreply.github.com>
2019-11-06 20:45:10 +0500

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,27 @@
+import random
+A=random.randint(-100, 100)
+ print("A=" + str(A))
+
+ B=random.randint(-100, 100)
+ print("B=" +str(B))
+
+ C=random.randint(-100,100)
+print("C=" +str(C) + "\n")
+
+
+ print("A<B<C and A>B>C""\n")
+A<B<C and A>B>C
+
+ if A<B and B<C:
+	print("The first inequality is true")
+	else A>B or B>C:
+		
+
+ print("The first inequality is fals")
+
+ if A>B and B>C:
+	print("The second inequality is true")
+	else A<B or B<C:
+		
+
+print("The second inequality is false")
