063a39071a4450a9cd178d017ab4a722210340b0
linberli <55867860+linberli@users.noreply.github.com>
2019-11-06 21:19:34 +0500

    Add files via upload

---
+++ 26.py
@@ -0,0 +1,12 @@
+a = input (' введите значение a ')
+ введите значение a 
+>>> x = a
+>>> if x <= 0 :
+	fx = 0
+	print (fx , '= ')
+elif 0<x<1 :
+	fx = x
+	print (fx , '= ')
+else :
+	fx = x ** 4
+	print ( fx, '= ')
---
+++ 27.py
@@ -0,0 +1,12 @@
+a = input (' введите значение a')
+ введите значение a
+>>> x = a
+>>> if x <= 0 :
+	fx = 0
+	print ( fx, '= ')
+elif 0<x<1 :
+	fx = x ** 2 - x
+	print ( fx, '= ')
+else :
+	fx = x ** 2 - sin( math.pi * x ** 2)
+	print (fx , '= ')
