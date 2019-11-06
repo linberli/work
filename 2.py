2fc7f246baddcf6829afa89a95bef3084c662122
linberli <55867860+linberli@users.noreply.github.com>
2019-11-06 19:39:54 +0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,7 @@
+>>> A=15
+>>> B=9
+>>> C=22
+>>> print("Max-",max(A,B,C))
+Max- 22
+>>> print("Min-",min(A,B,C))
+Min- 9
---
+++ 3.py
@@ -0,0 +1,8 @@
+>>> import math
+>>> a = 15
+>>> b = 10
+>>> c = 25
+>>> p = (a+b+c)/2.0
+>>> S = ( p*(p-a)*(p-b)*(p-c))
+>>> print(p)
+>>> print(S)
