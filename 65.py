cf80c09c172adc5cd4bd79113dea694a7ae0d19c
linberli <55867860+linberli@users.noreply.github.com>
2019-12-27 00:10:31 +0500

    Create 65.py

---
+++ 65.py
@@ -0,0 +1,19 @@
+#Вводятся положительные числа. 
+#Определить сумму чисел, делящихся на положительное число B нацело. 
+#При вводе отрицательного числа закончить работу.
+
+import random
+B=random.randint(1,10)
+print("B= "+str(B))
+M=random.randint(1,4)
+arr=[random.randint(1,10) for i in range(M)]
+a=0
+for i in range(M):
+    arr[i]=input()
+for i in range(M):
+    if int(arr[i])<0:
+        print("end")
+        break
+    elif int(arr[i]) % B ==0:
+        a+=int(arr[i])
+print("конечный результат"+str(a))
